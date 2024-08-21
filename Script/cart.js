8dd32f766c742998e1ee02cd6ff41c9f2c901329
Tausifqureshi <tausifqureshi504@gmail.com>
2024-08-21 21:45:40 +0530

    Project-File Push

--- Script/cart.js
+++ Script/cart.js
@@ -12,54 +12,20 @@ function updateTotalPrice() {
   totalPrice.classList.add("removePriceTotal");
 }
 
-function renderItems(products) {
-  const container = document.querySelector(".box-container");
-
-  container.innerHTML = ""; // Clear the container to prevent duplicate items
-
-  products.forEach((ele, index) => {
-    const box = document.createElement("div");
-    box.className = "box";
-
-    // Create and append Image
-    const img = document.createElement("img");
-    img.src = ele.image_url;
-    // box.appendChild(img);
-
-    // Create and append Title
-    const name = document.createElement("p");
-    name.innerText = ele.name;
-    name.className = "name";
-    // box.appendChild(title);
-    // Create and append Title
-    const des = document.createElement("p");
-    des.innerText = ele.description.slice(0,40);
-    des.className = "des";
-    // box.appendChild(title);
-
-    // Create and append Price
-    const price = document.createElement("p");
-    price.innerText = `$${ele.price}`;
-    price.className = "price";
-    // box.appendChild(price);
-
-    // Create and append Cart Trash Icon
-    const cartTrash = document.createElement("i");
-    cartTrash.className = "fa-sharp fa-solid fa-trash";
-    cartTrash.addEventListener("click", () => removeItem(index));
-    // box.append(title,cartTrash,img,price);
-    box.append(img,cartTrash,price,name,des);
-
-    container.append(box);
-  });
-
-  updateTotalPrice();
-}
+// 
 function renderItems() {
   const container = document.querySelector(".box-container");
   container.innerHTML = ""; // Clear the container to prevent duplicate items
 
-  products.forEach((product, index) => {
+  if (products.length === 0) {
+    // Agar cart empty hai toh message dikhaye
+    const emptyMessage = document.createElement("p");
+    emptyMessage.innerText = "Cart is empty";
+    emptyMessage.className = "empty-message";
+    container.append(emptyMessage);
+    totalPrice.textContent = ""; // Empty cart hone par total price ko bhi clear kar do
+  } else {
+    products.forEach((product, index) => {
       const box = document.createElement("div");
       box.className = "box";
 
@@ -84,30 +50,22 @@ function renderItems() {
 
       box.append(img, cartTrash, price, name, description);
       container.append(box);
-  });
+    });
 
-  updateTotalPrice();
+    updateTotalPrice();
+  }
+
+  
 }
+
 
 function removeItem(index) {
   products.splice(index, 1);
   localStorage.setItem("CartData", JSON.stringify(products));
   renderItems();
 }
-// function removeItem(index) {
-//   productsItems.splice(index, 1);
-//   // console.log(productsItems);
-//   localStorage.setItem("CartData", JSON.stringify(productsItems));
-//   renderItems(productsItems);
-// }
 
-// function removeItem(index, productsItems) {
-//   productsItems.splice(index, 1);
-//   localStorage.setItem("CartData", JSON.stringify(productsItems));
-//   renderItems(productsItems);
-// }
 
-// Assuming `productsGet` is defined elsewhere and contains the products data
 renderItems();
 
 // Scroll Bar
@@ -123,3 +81,50 @@ function scrollTop() {
 }
 
 scrollElemnt.addEventListener("click", scrollTop);
+
+
+
+// function renderItems(products) {
+  //   const container = document.querySelector(".box-container");
+  
+  //   container.innerHTML = ""; // Clear the container to prevent duplicate items
+  
+  //   products.forEach((ele, index) => {
+  //     const box = document.createElement("div");
+  //     box.className = "box";
+  
+  //     // Create and append Image
+  //     const img = document.createElement("img");
+  //     img.src = ele.image_url;
+  //     // box.appendChild(img);
+  
+  //     // Create and append Title
+  //     const name = document.createElement("p");
+  //     name.innerText = ele.name;
+  //     name.className = "name";
+  //     // box.appendChild(title);
+  //     // Create and append Title
+  //     const des = document.createElement("p");
+  //     des.innerText = ele.description.slice(0,40);
+  //     des.className = "des";
+  //     // box.appendChild(title);
+  
+  //     // Create and append Price
+  //     const price = document.createElement("p");
+  //     price.innerText = `$${ele.price}`;
+  //     price.className = "price";
+  //     // box.appendChild(price);
+  
+  //     // Create and append Cart Trash Icon
+  //     const cartTrash = document.createElement("i");
+  //     cartTrash.className = "fa-sharp fa-solid fa-trash";
+  //     cartTrash.addEventListener("click", () => removeItem(index));
+  //     // box.append(title,cartTrash,img,price);
+  //     box.append(img,cartTrash,price,name,des);
+  
+  //     container.append(box);
+  //   });
+  
+  //   updateTotalPrice();
+  // }
+  
