fb3b0f3d468a19ceedf5ac6842f2b7643f7e0819
Tausifqureshi <tausifqureshi504@gmail.com>
2025-07-27 00:22:59 +0530

    Tailwind-Use

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // content: [],
-    content: ["*"],
+  // content: ["*"],
+  content: ["./**/*.html"], // ✅ all HTML files in any folder
+  // content: ["./*.html"], // ✅ all root-level HTML files
 
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
