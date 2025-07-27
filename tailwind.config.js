e072df9a666c421dc992b069dd9629192937a606
Tausifqureshi <tausifqureshi504@gmail.com>
2025-07-27 18:14:14 +0530

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,5 @@ module.exports = {
 
 
 // jab bhi live server run kar ke project open kar re ge tab yaha command run karna padega.
-// npx tailwindcss -i ./input.css -o ./dist/output.css --watch
+//npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
+
